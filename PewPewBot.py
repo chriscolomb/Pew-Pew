e949337547ba9f357c784a32fc673e65a06ecc7b
CalmingAgent <65992597+CalmingAgent@users.noreply.github.com>
2021-01-02 17:43:09 -0500

    Got image added, can change later. created global variable for fight image.

--- PewPewBot.py
+++ PewPewBot.py
@@ -5,6 +5,8 @@ import PewPewDatabaseAccess
 
 
 def pew_bot_start(connection, cursor_name, client):
+    # image for confirming fight matchmaking
+    fight_image = 'PewPewBot_FinishHim.jpg'
 
     @client.event
     async def on_message(message):
@@ -14,18 +16,19 @@ def pew_bot_start(connection, cursor_name, client):
         if message.content.startswith('!add'):
             await message.channel.send('added wins to user')
             PewPewDatabaseAccess.update_values_columns('DamagedTwitch', connection, cursor_name, 'w')
+
     @client.event
     async def on_message(message):
         dragon = client.get_emoji(794999307048321044)
         if message.content.startswith('!match'):
             channel = message.channel
-            await channel.send('Send me that 👍 reaction, mate')
-
+            # enter user, get user id @ here and place it in match.
+            await channel.send('ready for a match? user, reply with kirby knife to confirm',
+                               file=discord.File(fight_image))
 
             def check(reaction, user):
                 # without custom, can do str(reaction.emoji)=='👎'
                 return user == message.author and reaction.emoji == dragon
-
 
             try:
                 reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
