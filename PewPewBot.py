7a68f62297b1f7a2258ae6e34e98b48adca3cd85
CalmingAgent <65992597+CalmingAgent@users.noreply.github.com>
2021-01-02 15:49:31 -0500

    custom emote reaction working

--- PewPewBot.py
+++ PewPewBot.py
@@ -1,8 +1,11 @@
+import asyncio
+
 import discord
 import PewPewDatabaseAccess
 
 
 def pew_bot_start(connection, cursor_name, client):
+
     @client.event
     async def on_message(message):
         if message.author == client.user:
@@ -11,3 +14,22 @@ def pew_bot_start(connection, cursor_name, client):
         if message.content.startswith('!add'):
             await message.channel.send('added wins to user')
             PewPewDatabaseAccess.update_values_columns('DamagedTwitch', connection, cursor_name, 'w')
+    @client.event
+    async def on_message(message):
+        dragon = client.get_emoji(794999307048321044)
+        if message.content.startswith('!match'):
+            channel = message.channel
+            await channel.send('Send me that 👍 reaction, mate')
+
+
+            def check(reaction, user):
+                # without custom, can do str(reaction.emoji)=='👎'
+                return user == message.author and reaction.emoji == dragon
+
+
+            try:
+                reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+            except asyncio.TimeoutError:
+                await channel.send('👎')
+            else:
+                await channel.send(dragon)
