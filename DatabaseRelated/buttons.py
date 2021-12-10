28f8a93b7efd38bdb666d8d007e810ef0277fdb9
CalmingAgent <65992597+CalmingAgent@users.noreply.github.com>
2021-12-10 03:34:12 -0500

    added disable button

--- DatabaseRelated/buttons.py
+++ DatabaseRelated/buttons.py
@@ -33,34 +33,40 @@ class AttackButtons(nextcord.ui.View):
 class WinorLose(nextcord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
+        #self.clicks = clicks
 
     #Where the magic will happen; the buttons will call the updateELO class from here, also need to disable buttons here
     async def handle_win_or_lose(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
         #placeholder, this is where the button will interact with the database
-        win_button.disabled == True
-        loss_button.disabled == True
+        button.disabled = True
+        
+        
 
     #button for winning
-    @nextcord.ui.button(label= "win", emoji="<:Cutedragon:794999307048321044>", style= nextcord.ButtonStyle.green, custom_id= "Iwin01")
+    @nextcord.ui.button(label= "win", disabled = False, emoji="<:Cutedragon:794999307048321044>", style= nextcord.ButtonStyle.green, custom_id= "Iwin01")
     async def win_button(self, button, interaction):
         get_user = interaction.user.id
-        await interaction.response.send_message("Lucky you")
+        #await interaction.response.send_message("Lucky you")
         await self.handle_win_or_lose(button,interaction)
+        await interaction.response.edit_message(view=self)
     #button for losing
     @nextcord.ui.button(label= "lose", emoji="<:Cutedragon:794999307048321044>", style= nextcord.ButtonStyle.green, custom_id= "Ilose01")
     async def loss_button(self, button, interaction):
         get_user = interaction.user.id
-        await interaction.response.send_message("I'm sorry for your loss")
+        #await interaction.response.send_message("I'm sorry for your loss")
+        await self.handle_win_or_lose(button,interaction)
+        await interaction.response.edit_message(view=self)
     
     #button for dispute
     @nextcord.ui.button(label= "dispute", emoji="<:Cutedragon:794999307048321044>", style= nextcord.ButtonStyle.green, custom_id= "dispute01")
     async def dispute_button(self, button, interaction):
         get_user = interaction.user.id
         await interaction.response.send_message("uhoh")
-    #button for confirm, could timeout this then erase after 1 minute
+    #button for confirm, could erase battle query from here
     @nextcord.ui.button(label= "confirm", emoji="<:Cutedragon:794999307048321044>", style= nextcord.ButtonStyle.green, custom_id= "confirm01")
     async def confirm_button(self, button, interaction):
         get_user = interaction.user.id
         await interaction.response.send_message("battle confirmed")
 
-    
+
+ 
