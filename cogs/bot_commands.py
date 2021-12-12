c9e312e4e094e492b9b280bbd19c788e3192a12a
CalmingAgent <65992597+CalmingAgent@users.noreply.github.com>
2021-12-12 02:13:43 -0500

    !fight adds players to db and continues to buttons

--- cogs/bot_commands.py
+++ cogs/bot_commands.py
@@ -65,27 +65,30 @@ class Bot_Commands(commands.Cog):
     async def fight(self,ctx, user: nextcord.Member):
         """initiates fight process"""
         not_in_db_count = 0
-        #checks if both users are in the database
-        for id in mongodb.player_collection.find():
-            if id["_id"] == user.id:
-                for idTwo in mongodb.player_collection.find():
-                    if idTwo["_id"] == ctx.author.id:
-                        await ctx.channel.send("both players found")
-                        p1_entry = Player(idTwo["_id"], rating=idTwo["rating"], win_count=idTwo["win_count"], lose_count=idTwo["lose_count"], win_streak=idTwo["win_streak"], best_win_streak=idTwo["best_win_streak"])
-                        p2_entry = Player(id["_id"], rating=id["rating"], win_count=id["win_count"], lose_count=id["lose_count"], win_streak=id["win_streak"], best_win_streak=id["best_win_streak"])
-                        await ctx.channel.send("Settle it in Smash.", view =AttackButtons(p1_entry, p2_entry))
-                        return
-                    else:
-                        not_in_db_count += 1
-                        author = ctx.author.id
+        entries_added = False
+        while not entries_added:
+            #checks if both users are in the database
+            for id in mongodb.player_collection.find():
+                if id["_id"] == user.id:
+                    for idTwo in mongodb.player_collection.find():
+                        if idTwo["_id"] == ctx.author.id:
+                            await ctx.channel.send("both players found")
+                            p1_entry = Player(idTwo["_id"], rating=idTwo["rating"], win_count=idTwo["win_count"], lose_count=idTwo["lose_count"], win_streak=idTwo["win_streak"], best_win_streak=idTwo["best_win_streak"])
+                            p2_entry = Player(id["_id"], rating=id["rating"], win_count=id["win_count"], lose_count=id["lose_count"], win_streak=id["win_streak"], best_win_streak=id["best_win_streak"])
+                            entries_added = True
+                            await ctx.channel.send("Settle it in Smash.", view =AttackButtons(p1_entry, p2_entry))
+                            return
+                        else:
+                            not_in_db_count += 1
+                            author = ctx.author.id
+                else:
+                    not_in_db_count += 1
+                    author = user.id
+            if not_in_db_count == 1:
+                editdatabase.EditDatabase.createPlayer(author)
             else:
-                not_in_db_count += 1
-                author = user.id
-        if not_in_db_count == 1:
-            editdatabase.EditDatabase.createPlayer(author)
-        else:
-            editdatabase.EditDatabase.createPlayer(ctx.author.id)
-            editdatabase.EditDatabase.createPlayer(user.id)
+                editdatabase.EditDatabase.createPlayer(ctx.author.id)
+                editdatabase.EditDatabase.createPlayer(user.id)
 
         
         await ctx.channel.send("added user to database")
