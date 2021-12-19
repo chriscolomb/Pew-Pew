50b1e86eabc47825474e71cd703fb281cb757433
chriscolomb <colomb.code@gmail.com>
2021-12-19 02:12:54 -0800

    Added !rankings command and design tweeks

--- cogs/bot_commands.py
+++ cogs/bot_commands.py
@@ -10,6 +10,7 @@ from buttons import AttackButtons
 from buttons import WinorLose 
 import editdatabase
 import random
+from datetime import datetime as dt
 
 #Notes:
 #Win and lose does not work, lines 82 to 92 on botcommands and 24 to 33 in buttons contribute to this
@@ -27,7 +28,7 @@ class Bot_Commands(commands.Cog):
         if user != None:
             for id in mongodb.player_collection.find():
                 if id["_id"] == user.id:
-                    title = "Stats for {0}".format(user)
+                    title = "Stats for {0}".format(user)[:-5]
 
                     embed = nextcord.Embed(
                         title = title,
@@ -40,12 +41,14 @@ class Bot_Commands(commands.Cog):
                     embed.add_field(name="Win Streak", value=id.get("win_streak"))
                     embed.add_field(name="Best Win Streak", value=id.get("best_win_streak"))
                     
+                    embed.set_footer(text="Generated on " + dt.now().strftime("%m/%d/%y at %I:%M %p"))
+                    
                     await ctx.channel.send(embed = embed)
                     return
         else:
             for id in mongodb.player_collection.find():
                 if id["_id"] == ctx.author.id:
-                    title = "Stats for {0.author}".format(ctx)
+                    title = "Stats for {0.author}".format(ctx)[:-5]
 
                     embed = nextcord.Embed(
                         title = title,
@@ -57,6 +60,8 @@ class Bot_Commands(commands.Cog):
                     embed.add_field(name="Lose Count", value=id.get("lose_count"))
                     embed.add_field(name="Win Streak", value=id.get("win_streak"))
                     embed.add_field(name="Best Win Streak", value=id.get("best_win_streak"))
+
+                    embed.set_footer(text="Generated on " + dt.now().strftime("%m/%d/%y at %I:%M %p"))
 
                     await ctx.channel.send(embed = embed)
                     return
@@ -131,7 +136,7 @@ class Bot_Commands(commands.Cog):
 
     @commands.command()
     async def random(self, ctx):
-        fighters = ['mario', 'donkey_kong', 'link', 'samus', 'dark_samus', 'yoshi', 'kirby', 'fox', 'pikachu', 'luigi', 'ness', 'captain_falcon', 'jigglypuff', 'peach', 'daisy', 'bowser', 'ice_climbers', 'sheik', 'zelda', 'dr_mario','pichu', 'falco', 'marth', 'lucina', 'young_link', 'ganondorf', 'mewtwo', 'roy', 'chrom','mr_game_and_watch', 'meta_knight', 'pit', 'dark_pit', 'zero_suit_samus', 'wario', 'snake', 'ike','pokemon_trainer', 'diddy_kong', 'lucas', 'sonic', 'king_dedede', 'olimar', 'lucario', 'rob', 'toon_link','wolf', 'villager', 'mega_man', 'wii_fit_trainer', 'rosalina_and_luma', 'little_mac', 'greninja','mii_fighter', 'palutena', 'pac_man', 'robin', 'shulk', 'bowser_jr', 'duck_hunt', 'ryu', 'ken', 'cloud','corrin', 'bayonetta', 'inkling', 'ridley', 'simon', 'richter', 'king_k_rool', 'isabelle', 'incineroar','piranha_plant', 'joker', 'dq_hero', 'banjo_and_kazooie', 'terry', 'byleth', 'minmin', 'steve', 'sephiroth', 'pyra', 'kazuya', 'sora']
+        fighters = ['mario', 'donkey_kong', 'link', 'samus', 'dark_samus', 'yoshi', 'kirby', 'fox', 'pikachu', 'luigi', 'ness', 'captain_falcon', 'jigglypuff', 'peach', 'daisy', 'bowser', 'ice_climbers', 'sheik', 'zelda', 'dr_mario','pichu', 'falco', 'marth', 'lucina', 'young_link', 'ganondorf', 'mewtwo', 'roy', 'chrom','mr_game_and_watch', 'meta_knight', 'pit', 'dark_pit', 'zero_suit_samus', 'wario', 'snake', 'ike','pokemon_trainer', 'diddy_kong', 'lucas', 'sonic', 'king_dedede', 'olimar', 'lucario', 'rob', 'toon_link','wolf', 'villager', 'mega_man', 'wii_fit_trainer', 'rosalina_and_luma', 'little_mac', 'greninja', 'mii_brawler', 'mii_gunner', 'mii_swordfighter', 'palutena', 'pac_man', 'robin', 'shulk', 'bowser_jr', 'duck_hunt', 'ryu', 'ken', 'cloud','corrin', 'bayonetta', 'inkling', 'ridley', 'simon', 'richter', 'king_k_rool', 'isabelle', 'incineroar','piranha_plant', 'joker', 'dq_hero', 'banjo_and_kazooie', 'terry', 'byleth', 'minmin', 'steve', 'sephiroth', 'pyra', 'kazuya', 'sora']
 
         alts = ['main', 'main2', 'main3', 'main4', 'main5', 'main6', 'main7', 'main8']
 
@@ -139,15 +144,94 @@ class Bot_Commands(commands.Cog):
 
         url = "https://raw.githubusercontent.com/chriscolomb/ssbu/master/OPTIMIZED%20PORTRAITS/"
         for fighter in fighters:
-            if fighter != 'mii_fighter':
+            if "mii" not in fighter:
                 for alt in alts:
                     image_url = url + fighter + '_' + alt + '.png'
                     character_images.append(image_url)
-            elif fighter == 'mii_fighter':
-                image_url = url + fighter + '_' + alts[0] + '.png'
+            else:
+                image_url = url + fighter + '.png'
                 character_images.append(image_url)
         
-        await ctx.channel.send(content=random.choice(character_images))
+        embed = nextcord.Embed()
+        embed.set_image(url=random.choice(character_images))
+        embed.colour = nextcord.Colour.from_rgb(121,180,183)
+        await ctx.channel.send(embed=embed)
+    
+
+    @commands.command()
+    async def rankings(self,ctx):
+        rankings = []
+        player = {
+            "id": None,
+            "rating": 0,
+            "tier": None
+        }
+
+        for id in mongodb.player_collection.find().sort("rating", -1):
+            player["id"] = id["_id"]
+            player["rating"] = id["rating"]
+            player["tier"] = Player.get_tier(player["rating"])
+            rankings.append(player.copy())
+            player.clear()
+
+        embed = nextcord.Embed(
+            title = "Rankings",
+            colour = nextcord.Colour.from_rgb(121,180,183)
+        )
+        embed.set_footer(text="Generated on " + dt.now().strftime("%m/%d/%y at %I:%M %p"))
+
+        diamond_rankings = []
+        platinum_rankings = []
+        gold_rankings = []
+        silver_rankings = []
+        bronze_rankings = []
+        for element in rankings:
+            if element["tier"] == "Diamond":
+                diamond_rankings.append(element)
+            elif element["tier"] == "Platinum":
+                platinum_rankings.append(element)
+            elif element["tier"] == "Gold":
+                gold_rankings.append(element)
+            elif element["tier"] == "Silver":
+                silver_rankings.append(element)
+            elif element["tier"] == "Bronze":
+                bronze_rankings.append(element)
+        
+        rank = 1
+        if diamond_rankings:
+            diamond_value = ""
+            for element in diamond_rankings:
+                diamond_value += str(rank) + ". " + str(self.client.get_user(element["id"]))[:-5] + " - `" + str(element["rating"]) + "`\n"
+                rank += 1
+            embed.add_field(name="Diamond", value=diamond_value)
+        if platinum_rankings:
+            platinum_value = ""
+            for element in platinum_rankings:
+                platinum_value += str(rank) + ". " + str(self.client.get_user(element["id"]))[:-5] + " - `" + str(element["rating"]) + "`\n"
+                rank += 1
+            embed.add_field(name="Platinum", value=platinum_value)
+        if gold_rankings:
+            gold_value = ""
+            for element in gold_rankings:
+                gold_value += str(rank) + ". " + str(self.client.get_user(element["id"]))[:-5] + " - `" + str(element["rating"]) + "`\n"
+                rank += 1
+            embed.add_field(name="Gold", value=gold_value)
+        if silver_rankings:
+            silver_value = ""
+            for element in silver_rankings:
+                silver_value += str(rank) + ". " + str(self.client.get_user(element["id"]))[:-5] + " - `" + str(element["rating"]) + "`\n"
+                rank += 1
+            embed.add_field(name="Silver", value=silver_value)
+        if bronze_rankings:
+            bronze_value = ""
+            for element in bronze_rankings:
+                bronze_value += str(rank) + ". " + str(self.client.get_user(element["id"]))[:-5] + " - `" + str(element["rating"]) + "`\n"
+                rank += 1
+            embed.add_field(name="Bronze", value=bronze_value)
+
+        await ctx.channel.send(embed = embed)
+
+        
 
 
 def setup(client):
