f9524811d8dbe5c5ff81d703963d8df1b92692ef
CalmingAgent <65992597+CalmingAgent@users.noreply.github.com>
2021-12-19 17:02:28 -0500

    commands fixed plus added Chris's code

--- cogs/admin_commands.py
+++ cogs/admin_commands.py
@@ -1,28 +1,31 @@
+from os import close
 import nextcord
 import sys
 from nextcord.ext import commands
 
 #parent directory import
 sys.path.append('DatabaseRelated')
+sys.path.append('cogs')
 import mongodb
 from player import Player
 from buttons import AttackButtons
 from buttons import WinorLose
 from buttons import MatchComplete
+from bot_commands import Bot_Commands
 # this would be used for only admin in TTD server, need to implement this
 class Admin_Commands(commands.Cog):
     """Admin Commands"""
 
     def __init__(self,client: commands.Bot):
         self.client = client
-        
-    
+                
     @commands.Cog.listener()
     async def on_ready(self):
         print("bot ready")
         self.client.add_view(AttackButtons())
         self.client.add_view(WinorLose())
         self.client.add_view(MatchComplete())
+
         #print('Logged on as {0}!'.format(self.user.name))
 
     @commands.command()
@@ -67,7 +70,18 @@ class Admin_Commands(commands.Cog):
         else:
             await ctx.channel.send("You don't have admin privilages to do this command")
     
-    
+    @commands.command()
+    async def write_emojis(self,ctx):
+        """writes emojis into a text file"""
+        if ctx.author.guild_permissions.administrator:
+            server_emojis = open("server_emojis.txt", "w")
+            #need to change this to TTD server ID
+            server = self.client.get_guild(575869943346757682)
+            for emoji in server.emojis:
+                character = [str(emoji.name), " ", str(emoji.id), "\n"]
+                server_emojis.writelines(character)
+            await ctx.channel.send("txt file updated")
+            server_emojis,close()
 
 def setup(client):
     client.add_cog(Admin_Commands(client))
